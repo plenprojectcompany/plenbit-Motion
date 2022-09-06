749c4f9200ed900ea3083c0d6e4eb929d1421cf8
IsakiMatsubara <61609988+IsakiMatsubara@users.noreply.github.com>
2022-09-06 16:55:34 +0900

    fix color

--- sdk/motion-writer.py
+++ sdk/motion-writer.py
@@ -16,6 +16,16 @@ import serial
 import serial.tools.list_ports
 import time
 from protocol import Protocol
+import ctypes
+ 
+ENABLE_PROCESSED_OUTPUT = 0x0001
+ENABLE_WRAP_AT_EOL_OUTPUT = 0x0002
+ENABLE_VIRTUAL_TERMINAL_PROCESSING = 0x0004
+MODE = ENABLE_PROCESSED_OUTPUT + ENABLE_WRAP_AT_EOL_OUTPUT + ENABLE_VIRTUAL_TERMINAL_PROCESSING
+ 
+kernel32 = ctypes.windll.kernel32
+handle = kernel32.GetStdHandle(-11)
+kernel32.SetConsoleMode(handle, MODE)
 
 class color:
     green = '\033[92m'
